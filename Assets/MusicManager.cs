3f0e301abf82807020eea4b4998a60faebfe523c
stevenevert <steve4xp@gmail.com>
2018-12-11 12:36:42 -0800

    Music added
    Added music files and created the beginning of scripts to handle audio
    clips/sounds

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public Song[] songs;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Song s in songs)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.Play();
+            while (s.source.isPlaying)
+            {
+
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Song.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Song {
+
+    public AudioClip clip;
+
+    public AudioSource source;
+
+
+
+}
