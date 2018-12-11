fba066bd9c779dc55e5bd5eed7b2fd26f2b1ef19
Matthew Tarantino <tarantinoma2@gmail.com>
2018-12-11 15:57:18 -0800

    Persistence Test Project
    Added a separate project to test data persistence without editing the master branch of our project.

---
+++ Persistence Test/Assets/Scripts/loadscene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class loadscene : MonoBehaviour {
+
+	// Use this for initialization
+	public void changeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
