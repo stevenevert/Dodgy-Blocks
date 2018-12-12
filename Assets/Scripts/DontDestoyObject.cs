5a9faafe8dbee563d8bc7e8d595eddd39a40fafb
stevenevert <steve4xp@gmail.com>
2018-12-12 14:49:58 -0800

    Persistent music
    added a script to the BackgroundMusic obeject so that it is not
    destroyed between scenes.

---
+++ Assets/Scripts/DontDestoyObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestoyObject : MonoBehaviour {
+
+	//Script to ensure that this object is not destroyed when moving to another scene
+	void Awake () {
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+
+}
