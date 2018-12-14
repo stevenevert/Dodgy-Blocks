fec14e933833b761ce80dc4efbdacb0069395a58
Kimara9 <kmaranon9@gmail.com>
2018-12-13 23:59:14 -0800

    Added a level and rotation script

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    [SerializeField] private Vector3 axis;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(axis * Time.deltaTime);
+	}
+}
