52f565528d0ff4d00a6e7b451396fa09cdb5a074
Kimara9 <kmaranon9@gmail.com>
2018-12-14 04:47:02 -0800

    Added menu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+    public string newGameScene;
+    public string howToPlay;
+    public string mainMenu;
+    
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+    public void howTo()
+    {
+        SceneManager.LoadScene(howToPlay);
+    }
+    public void titleMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+}
