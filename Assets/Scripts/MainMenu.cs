1714ff3d77c56a838c6cf431a41118ccf3506fab
Kimara9 <kmaranon9@gmail.com>
2018-12-14 04:58:39 -0800

    Configured a button on main menu with newly added script/ Script changed to fit current levels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,30 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour {
+public class mainMenu : MonoBehaviour {
 
-    public string newGameScene;
-    public string howToPlay;
-    public string mainMenu;
+    public string Level1;
+    public string Level2;
+    public string Level3;
+    public string Level4;
+    public string Level5;
+    public string Level6;
+  
+    public string firstMenu;
     
-    void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
-    public void NewGame()
+
+    public void level1()
     {
-        SceneManager.LoadScene(newGameScene);
+        SceneManager.LoadScene(Level1);
     }
-    public void howTo()
+    public void level2()
     {
-        SceneManager.LoadScene(howToPlay);
+        SceneManager.LoadScene(Level2);
     }
+    public void level3()
+    {
+        SceneManager.LoadScene(Level3);
+    }
+    public void level4()
+    {
+        SceneManager.LoadScene(Level4);
+    }
+    public void level5()
+    {
+        SceneManager.LoadScene(Level5);
+    }
+    public void level6()
+    {
+        SceneManager.LoadScene(Level6);
+    }
+
     public void titleMenu()
     {
-        SceneManager.LoadScene(mainMenu);
+        SceneManager.LoadScene(firstMenu);
     }
 
 }
