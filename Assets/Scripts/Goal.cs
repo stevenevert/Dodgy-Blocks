7c19938daa6dbb1749b6bea8d2601f6c5f767440
Matthew Tarantino <tarantinoma2@gmail.com>
2018-12-13 22:54:43 -0800

    Made persistence for each level
    added the new levels to the switch statement allowing the same script to be used on each level.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour {
 
@@ -11,22 +12,80 @@ public class Goal : MonoBehaviour {
     /// </summary>
     void Start () {
         gameWin.gameObject.SetActive(false);
+        //Scene currentScene = SceneManager.GetActiveScene();
+        //string currentName = currentScene.name;
     }
 	
 
     
     void OnCollisionEnter2D(Collision2D other)
     {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string currentName = currentScene.name;
         if (other.transform.CompareTag("Player"))
         {
-            if (deathControl.deaths < gameControl.Lvl1Deaths)
+            switch (currentName)
+            {
+                case "Level_Eric":
+                    if (deathControl.deaths < gameControl.Lvl1Deaths)
+                    {
+                        gameControl.Lvl1Deaths = deathControl.deaths;
+                    }
+                    if (Countdown.timer < gameControl.Lvl1Time)
+                    {
+                        gameControl.Lvl1Time = Countdown.timer;
+                    }
+                    break;
+                case "Level_Steven":
+                    if (deathControl.deaths < gameControl.Lvl2Deaths)
+                    {
+                        gameControl.Lvl2Deaths = deathControl.deaths;
+                    }
+                    if (Countdown.timer < gameControl.Lvl2Time)
+                    {
+                        gameControl.Lvl2Time = Countdown.timer;
+                    }
+                    break;
+                case "Level_Dominick":
+                    if (deathControl.deaths < gameControl.Lvl3Deaths)
+                    {
+                        gameControl.Lvl3Deaths = deathControl.deaths;
+                    }
+                    if (Countdown.timer < gameControl.Lvl3Time)
+                    {
+                        gameControl.Lvl3Time = Countdown.timer;
+                    }
+                    break;
+                case "Level_Matthew":
+                    if (deathControl.deaths < gameControl.Lvl4Deaths)
+                    {
+                        gameControl.Lvl4Deaths = deathControl.deaths;
+                    }
+                    if (Countdown.timer < gameControl.Lvl4Time)
+                    {
+                        gameControl.Lvl4Time = Countdown.timer;
+                    }
+                    break;
+                case "Level_Kian":
+                    if (deathControl.deaths < gameControl.Lvl5Deaths)
+                    {
+                        gameControl.Lvl5Deaths = deathControl.deaths;
+                    }
+                    if (Countdown.timer < gameControl.Lvl5Time)
+                    {
+                        gameControl.Lvl5Time = Countdown.timer;
+                    }
+                    break;
+                default: break;
+            }
+           /* if (deathControl.deaths < gameControl.Lvl1Deaths)
             {
                 gameControl.Lvl1Deaths = deathControl.deaths;
             }
            if (Countdown.timer < gameControl.Lvl1Time)
            {
                 gameControl.Lvl1Time = Countdown.timer;
-           }
+           }*/
             gameWin.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
