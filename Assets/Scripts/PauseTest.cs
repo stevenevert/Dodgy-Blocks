f7610594a2f701cec70ad51de5edbeff94233dea
Kimara9 <kmaranon9@gmail.com>
2018-12-14 20:59:59 -0800

    Implemented pause

---
+++ Assets/Scripts/PauseTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseTest : MonoBehaviour {
+
+    private bool isPaused = false;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+                Time.timeScale = 1;
+            else
+                Time.timeScale = 0;
+        }
+	}
+}
