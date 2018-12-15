c61ccf68a51f9be790b0ab146ab6fe6a1f924757
stevenevert <steve4xp@gmail.com>
2018-12-14 23:20:03 -0800

    Music volume control
    The volume slider on the main menu now controls the music volume.  Also,
    the music now persists through each level.

--- Assets/Scripts/DontDestoyObject.cs
+++ Assets/Scripts/DontDestoyObject.cs
@@ -7,6 +7,9 @@ public class DontDestoyObject : MonoBehaviour {
 	//Script to ensure that this object is not destroyed when moving to another scene
 	void Awake () {
         DontDestroyOnLoad(transform.gameObject);
+    //don't create another background music if there already is one
+        if (FindObjectsOfType<DontDestoyObject>().Length > 1)
+            Destroy(gameObject);
 	}
 	
 
