d828b63afbed8ab521d8aa28430d919134f7355e
Dominick Sahagun <domoniick@gmail.com>
2018-12-16 18:01:38 -0800

    PauseMenu UI
    Pause menu pops on on esc key press. With buttons resume and main menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+
+    private static bool PauseGame = false;
+
+    public GameObject PauseMenuUI;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame = false;
+    }
+     void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    public void MoveMainMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("Menu");
+    }
+
+  
+}
